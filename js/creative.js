60c20804e7a030ee1a7849951ed9e11668c6f5dd
Diogo Trentini <diogotrentini@gmail.com>
2017-09-26 21:10:47 -0300

    Fix map

--- js/creative.js
+++ js/creative.js
@@ -47,3 +47,41 @@
 
 })(jQuery); // End of use strict
 
+function initMap() {
+  var MAP_STYLE_OPTIONS = [{
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{"lightness": 100}, {"visibility": "simplified"}]
+  }, {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{"visibility": "on"}, {"color": "#C6E2FF"}]
+  }, {
+    "featureType": "poi",
+    "elementType": "geometry.fill",
+    "stylers": [{"color": "#C5E3BF"}]
+  }, {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [{"color": "#D1D1B8"}]
+  }];
+
+  var MAP_OPTIONS = {
+    center: {lat: -26.33174875581043, lng: -48.904978625378405 },
+    zoom: 15,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    zoomControl: true,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+    }
+  };
+
+  var $mapContainer = $('.js-map')[0];
+  var styledMapType = new google.maps.StyledMapType(MAP_STYLE_OPTIONS);
+  var map = new google.maps.Map($mapContainer, MAP_OPTIONS);
+
+  new google.maps.Marker({position: MAP_OPTIONS['center'], map: map});
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
+}
